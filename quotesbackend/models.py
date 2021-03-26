1b4f250c08604d7eee2cfa0b1e332bec5d1182bb
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-03-26 14:36:40 -0600

    added database connection

--- quotesbackend/models.py
+++ quotesbackend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Quotes(models.Model):
+    title = models.CharField(max_length=50)
+    text_body = models.TextField()
+    img_url = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
