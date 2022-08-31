de5c00eb06dbb915027087824b3bff1e16f3851d
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-08-31 13:11:33 -0600

    DEBUG OFF

--- quotesdjango/settings.py
+++ quotesdjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y-#l(tgngi2wzok2@=ntv8=k0%=x^ne(ze8^i!0nqxmng&u&ba'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'quotesdjango.herokuapp.com', 'quotesdjango.up.railway.app']
 
